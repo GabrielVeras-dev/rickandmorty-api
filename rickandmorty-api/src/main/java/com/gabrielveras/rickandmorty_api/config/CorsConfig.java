2d43385b1af6cebd08bf303a9aabd13c292bac5f
Gabriel Veras <bielveras.e@hotmail.com>
2026-04-04 17:59:33 -0300

    fix: adicionado URL do Vercel no CORS

--- rickandmorty-api/src/main/java/com/gabrielveras/rickandmorty_api/config/CorsConfig.java
+++ rickandmorty-api/src/main/java/com/gabrielveras/rickandmorty_api/config/CorsConfig.java
@@ -4,14 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// Libera CORS para o Angular consumir a API localmente
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://devgabrielveras-apirickandmorty.vercel.app"
+                )
                 .allowedMethods("GET")
                 .allowedHeaders("*");
     }
